60e7a74d8965516b3663dcf117d055956cd2e756
yangjzh888 <365464311@qq.com>
2021-02-06 00:02:08 +0800

    create project

---
+++ src/com/yjz/test/Test.java
@@ -0,0 +1,49 @@
+package com.yjz.test;
+
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+
+    public static void main(String[] args) {
+        try {
+            // 创建并调整单例型的配置对象
+            Configuration cfg = new Configuration(Configuration.VERSION_2_3_30);
+            String path = Test.class.getResource("/").toURI().getPath();
+            cfg.setDirectoryForTemplateLoading(new File(path + "templates"));
+            cfg.setDefaultEncoding("UTF-8");
+            cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+
+            // 创建数据模型
+            Map root = new HashMap<>();
+            root.put("user", "Big Joe");
+            Map latest = new HashMap();
+            root.put("latestProduct", latest);
+            latest.put("url", "products/greenhouse.html");
+            latest.put("name", "green house");
+
+            // 获得模板
+            Template temp = cfg.getTemplate("aa.ftl");
+
+            // 填充模板
+            Writer out = new OutputStreamWriter(System.out);
+            temp.process(root, out);
+
+
+        } catch (URISyntaxException | IOException | TemplateException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/templates/aa.ftl
@@ -0,0 +1,8 @@
+<html>
+<head>Welcome!</head>
+<body>
+<h1>Welcome ${user}!</h1>
+<p>Our latest product: </p>
+<a href="${latestProduct.url}">${latestProduct.name}</a>
+</body>
+</html>
